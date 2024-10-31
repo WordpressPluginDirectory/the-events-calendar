1684eb1ffa18fc8ecb8d7e6ed37a307429d1133f
Justin Gardner <justin@rhynorater.com>
2024-10-31 06:01:40 -0500

    6.8.0

--- the-events-calendar/vendor/composer/installed.php
+++ the-events-calendar/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'the-events-calendar/the-events-calendar',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'b3d38b1fc85fa69372c5758c8e69dd91f18de017',
+        'reference' => '4483b8c37235f757640081fff478f5f741076ab1',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'the-events-calendar/the-events-calendar' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'b3d38b1fc85fa69372c5758c8e69dd91f18de017',
+            'reference' => '4483b8c37235f757640081fff478f5f741076ab1',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
