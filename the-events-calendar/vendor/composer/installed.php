ef165468bc0dfd4c24ff469e7d7f72f8288878fe
Justin Gardner <justin@rhynorater.com>
2025-07-04 06:02:10 -0500

    6.14.0

--- the-events-calendar/vendor/composer/installed.php
+++ the-events-calendar/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'the-events-calendar/the-events-calendar',
-        'pretty_version' => 'dev-release/T25.enchantress.1',
-        'version' => 'dev-release/T25.enchantress.1',
-        'reference' => 'ce40789fb5413bac5376fa8c0e5e0919cb1332d5',
+        'pretty_version' => 'dev-release/T25.flash',
+        'version' => 'dev-release/T25.flash',
+        'reference' => '23e387e1bc44da312488035de51f4fca71ba7764',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'the-events-calendar/the-events-calendar' => array(
-            'pretty_version' => 'dev-release/T25.enchantress.1',
-            'version' => 'dev-release/T25.enchantress.1',
-            'reference' => 'ce40789fb5413bac5376fa8c0e5e0919cb1332d5',
+            'pretty_version' => 'dev-release/T25.flash',
+            'version' => 'dev-release/T25.flash',
+            'reference' => '23e387e1bc44da312488035de51f4fca71ba7764',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
